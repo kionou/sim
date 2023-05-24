3cfc3bfdab6ad68098600683977240d06d75bf31
kionou <kionoumamadou.00@gmail.com>
2023-05-24 18:12:03 +0000

    ok

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get('/but', function(req, res, next) {
+  res.render('but',);
+});
+
+router.get('/match', function(req, res, next) {
+  res.render('match',);
 });
 
 module.exports = router;
